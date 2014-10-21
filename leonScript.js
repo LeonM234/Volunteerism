ac37a852cc38e3104abe345e35672c69c6edffdf
Leon Peck <leonpeck234@gmail.com>
2014-10-21 14:24:44 -0500

    Completed random-n implementation

--- leonScript.js
+++ leonScript.js
@@ -1,83 +1,101 @@
-var people = ['Bob', 'Sally', 'Joe', 'Susan', 'Leon', 'Spencer', 'Alex', 'Adam', 'Steve', 'Kris', 'Blaze', 'Cindy', 'Jessica', 'Fred', 'Jonathan', 'Ted', 'Bubba', 'Peter', 'Stanley', 'Fiona', 'Larry', 'Michael'];
+function getJSON(url, cb){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onload = function(){
+    cb(JSON.parse(xhr.responseText));
+  };
+  xhr.send();
+}
+
+//var students = ["Seif", "Jackie", "Jessica", "Kimberly", "Brandon", "Blaise", "Evan", "Greg", "Charisse", "Luke", "Stephania", "David", "Steve", "Adam", "Spencer", "Leon", "Alex", "Gerald", "Sonda", "Beck", "Colby", "Kris"];
+
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min (included) and max (excluded)
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function addItemToList($list, itemText){
+    var $li = document.createElement("li");
+    $li.innerHTML = itemText;
+    $list.appendChild($li);
+}
+
+
+function neighborGrouping(list, groupSize, target){
+  var listClone = list.slice(0);
+  while (listClone.length > 0){
+    var listItems = listClone.splice(0, groupSize);
+    addItemToList(target, listItems.join(" &amp; "));
+  }
+}
+
+function arrayShuffle(array){
+  var arrayClone = array.slice(0);
+  var temp;
+  for(var i = 0; i < arrayClone.length; i++){
+    var rand = getRandomInt(0, arrayClone.length);
+    temp = arrayClone[i];
+    arrayClone[i] = arrayClone[rand];
+    arrayClone[rand] = temp;
+  }
+  return arrayClone;
+}
+
+function show(element) {
+  element.classList.remove("hidden");
+}
+
+function hide(element) {
+  element.classList.add("hidden");
+}
+
+
 
 document.addEventListener('DOMContentLoaded', function(){
-var $rsSelected = document.getElementById('randStud');
-var $pickButton = document.getElementById('pick');
-var $orderedList = document.getElementById('order_list');
+  var $form = document.getElementById("generate-group");
 
+  var $select = $form.querySelector("select");
+  var $numBox = $form.querySelector("input[type='number']");
 
-  // 1.  Random Student
-  /* $pickButton.addEventListener('click', function(){
-      var number = Math.round(Math.random() * people.length);
-      var $randomStudent = people[number];
-      
-      var li = document.createElement('li');
-      li.innerHTML = randomStudent;
-      orderedList.appendChild(li); */
-  
-
-  // 2. Neighbor Pairing
-   /* pickButton.addEventListener('click', function(){
-    if (people.length > 0) {
-      var li = document.createElement('li');
-      li.innerHTML = people.splice(0, 1) + " & " + people.splice(0, 1);
-      orderedList.appendChild(li);
-    } */
-
-
-  // 3. Teams of Three
-    /*pickButton.addEventListener('click', function(){
-      if(people.length > 2) {
-        var li = document.createElement('li');
-        li.innerHTML = people.splice(0, 1) + " & " + people.splice(0, 1) + " & " + people.splice(0, 1);
-        orderedList.appendChild(li);
-    } else if (people.length === 1) {
-        var li = document.createElement('li');
-        li.innerHTML = people.splice(0, 1) + " is the last person, and does not have anyone to pair with."        
-        orderedList.appendChild(li);
-    }*/
-  
-  // 4. Random Pairing
-    $pickButton.addEventListener('click', function(){
-      // if (random pairing selected)
-      var peopleClone = people.slice(0);
-   
-      for (var i = 0; i < peopleClone.length; i += 2) {  
-      var number = Math.floor(Math.random() * peopleClone.length);
-      var randomStudent = peopleClone.splice(number, 1);
-      var number2 = Math.floor(Math.random() * peopleClone.length);
-      var randomStudent2 = peopleClone.splice(number2, 1);
-
-      var li = document.createElement('li');
-      li.innerHTML = randomStudent + " & " + randomStudent2;
-      $orderedList.appendChild(li);
+  $select.addEventListener("change", function(event){
+  if (event.currentTarget.value === "randN") {
+      show($numBox);
+    } else {
+      hide($numBox);
     }
   });
 
+  $form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    getJSON('https://volunteerism-sscotth.firebaseio.com/students.json', function(data){
+      var students = data;      
+
+      var $ol = document.getElementById('order-list');
+      $ol.innerHTML = "";
+
+      var groupCriteria = $form.querySelector("select").value;
+
+      if (groupCriteria === "random-student"){
+        var studentNumber = getRandomInt(0, students.length);
+        var studentName = students[studentNumber];
+        addItemToList($ol, studentName);
+      } else if (groupCriteria === "neighbor-pairing") {
+          neighborGrouping(students, 2, $ol);
+      } else if (groupCriteria === "teams-of-three"){
+          neighborGrouping(students, 3, $ol);
+      } else if (groupCriteria === "random-pairing"){
+          var shuffledStudents = arrayShuffle(students);
+          neighborGrouping(shuffledStudents, 2, $ol);
+      } else if (groupCriteria === "randN"){
+          var shuffledStudents = arrayShuffle(students);
+          neighborGrouping(shuffledStudents, $numBox.value, $ol);
+      }
+    }
+   );
   });
-  //});
-  //});
-// });
-
-// 1. Random Student TO-DO
-// XXXX -- Add event listener to when 'pick' is clicked
-// IF 'random student' is selected when 'pick is clicked
-  // XXXX -- Perform function using Math.random()
-  // XXXX -- Combine Math.random with Math.round to round numbers
-  // XXXX -- Make names appear on the webpage (via DOM appending)
-  // XXXX -- Append to Ordered List
-
-// 2. Neighbor Pairing TO-DO
-// XXXX -- Add event listener to when 'pick' is clicked
-// IF 'Neighbor Pairing' is selected when 'pick' is clicked
-// XXXX -- Figure out how to select a pair: slice? splice? Some other method? ??
-  // XXXX -- Display array pair by appending to the DOM
-  // XXXX -- Gets wonky towards the end, need to stop it when last pair is displayed.
+});
 
 
-// 3. Teams of Three
-  // Got it first time
-
-// 4. Random Pairing
-  // Pick a random student
-  // Add student to v
+ 
